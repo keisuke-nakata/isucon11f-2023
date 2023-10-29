9f006cf2063543e8958dddb8041f35da8faa0659
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2023-10-29 15:29:42 +0900

    mysql appserver3

--- makesvg.sh
+++ makesvg.sh
@@ -1,2 +1,2 @@
 export PATH=/home/isucon/bin/FlameGraph:$PATH
-go-torch -b $PPORF_DIR/cpu.pprof -f $PPORF_DIR/prof.svg
+go tool pprof -http=":8081" result/20231029-060647_6c5290da549609d179418cdf9b6e6512ae982a3b/appserver1/profile/cpu.pprof
